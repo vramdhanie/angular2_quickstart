ee40c6a31b4718fa78a32746238f45975e8c9dda
Vincent Ramdhanie <vramdhanie@gmail.com>
2016-10-10 10:33:45 -0400

    Add a module for our application

---
+++ app/app.module.js
@@ -0,0 +1,11 @@
+(function(){
+    app.AppModule =
+        ng.core.NgModule({
+            imports: [ng.platformBrowser.BrowserModule],
+            declarations: [app.AppComponent],
+            botstrap: [app.AppComponent]
+        })
+        .Class({
+            constructor: function(){}
+        });
+})(window.app || (window.app = {}));
